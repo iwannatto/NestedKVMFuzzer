748495b2ac020cc05e1f7917e6c98fa45860e540
iwannatto <iwannatto@gmail.com>
2021-06-03 18:29:25 +0900

    First tmp commit

---
+++ fuzzer/run.sh
@@ -0,0 +1,14 @@
+# sudo bash -c 'echo core >/proc/sys/kernel/core_pattern'
+
+set -e
+
+cd $(dirname $0)
+
+cd AFLplusplus; make; cd ..
+make fuzznetlink
+
+rm debugf.txt
+
+AFL_I_DONT_CARE_ABOUT_MISSING_CRASHES=1 \
+    AFL_SKIP_CPUFREQ=1 \
+    ./AFLplusplus/afl-fuzz -i ./inp -o ./out -- ./fuzznetlink --dmesg dmesg.txt

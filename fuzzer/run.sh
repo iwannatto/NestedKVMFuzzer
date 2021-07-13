bea112e97752e0e8ba0ec5c5a319dbd1304a6932
iwannatto <iwannatto@gmail.com>
2021-07-13 21:50:35 +0900

    Use current datetime for output directory name and Fix timeout of AFL

--- fuzzer/run.sh
+++ fuzzer/run.sh
@@ -4,17 +4,24 @@ set -eux
 
 cd $(dirname $0)
 
+# build each component
+
 cd AFLplusplus; make source-only; cd ..
 make fuzznetlink
 ./qemu/build.sh
 cd VMXbench; make; cd ..
 
+# run
+
 rm -f debugf_fuzznetlink.txt
+
+readonly current_datetime="$(date '+%Y%m%d_%H%M%S')"
+readonly out_dir=./results/"${current_datetime}"
 
 sudo \
     AFL_I_DONT_CARE_ABOUT_MISSING_CRASHES=1 \
     AFL_SKIP_CPUFREQ=1 \
-    ./AFLplusplus/afl-fuzz -i ./inp -o ./out -t 20000 -- ./fuzznetlink
+    ./AFLplusplus/afl-fuzz -i ./inp -o "${out_dir}" -t 30000 -- ./fuzznetlink
 
 # sudo \
 #     AFL_I_DONT_CARE_ABOUT_MISSING_CRASHES=1 \

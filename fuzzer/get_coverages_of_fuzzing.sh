d0f6d381efab4dc91ad8da8a8316e9dfb9b3e765
iwannatto <iwannatto@gmail.com>
2022-01-09 20:46:01 +0900

    Fix argument of a script

--- fuzzer/get_coverages_of_fuzzing.sh
+++ fuzzer/get_coverages_of_fuzzing.sh
@@ -1,6 +1,11 @@
 set -eux
 
-readonly fuzzing_result_dir='results/20211231_201627'
+if [[ "$#" -ne 1 ]]; then
+    echo "exactly 1 argument (fuzzing_result_dir) is required, but given $#" 1>&2
+    exit 1
+fi
+
+readonly fuzzing_result_dir="$1"
 
 for input in "${fuzzing_result_dir}/default/queue/id:"*; do
     ./run_standalone_qemu_wrapper.sh "${input}" "${fuzzing_result_dir}/coverage/$(basename ${input}).bin"
